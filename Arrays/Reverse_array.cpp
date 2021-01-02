d6d9dacf852075a54f5dbeec19d0a93fe4946e7d
Vansh <44532230+aroravansh@users.noreply.github.com>
2021-01-02 20:32:10 +0530

    Array Rearrangment
    Basic program to reverse an array in linear time
    Time Complexity: O(n)

---
+++ Arrays/Reverse_array.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;        //size of array
+	cin>>n;
+	int arr[n];    //array of size n
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];    //input
+    }
+    int j=n-1;     //acting as a pointer
+    for(int i=0;i<n/2;i++)
+    {
+        int tmp = arr[i];
+        arr[i] = arr[j];       //swapping elements using tmp variable
+        arr[j--] = tmp;
+    }
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";   
+    }
+    
+	return 0;
+}
