3997a7ce988f71b5dc38f52842708966a4c0fceb
Vansh <44532230+aroravansh@users.noreply.github.com>
2021-01-04 00:41:06 +0530

    Array Rearrangement
    Program to cyclically rotate array by one.
    Time Complexity: O(n)

---
+++ Arrays/Rotate_1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;        //size of array
+	cin>>n;
+	int arr[n];    //array of size n
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];    //input
+    }
+    int c=arr[0];        
+    for(int i=0;i<n-1;i++)
+    {
+        arr[i] = arr[i+1];    //shifting
+    }
+    arr[n-1] = c;     //for last element
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";      //output
+    }
+	return 0;
+}
