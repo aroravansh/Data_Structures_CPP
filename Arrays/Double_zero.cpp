036301702402b70ca30f8cb1238b64cb3d3d8524
Vansh <44532230+aroravansh@users.noreply.github.com>
2021-01-05 23:57:52 +0530

    Order statistics
    Program to move all zeroes to the end and if 2 non zero elemets are together, double the first and make the second one zero
    Time Complexity: O(n)

---
+++ Arrays/Double_zero.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	int a[n];   //array given
+	for(int i=0;i<n;i++)
+	{
+	    cin>>a[i];
+	}
+    int k =0;
+	for(int i=0;i<n;i++)
+	{
+	    if(i<n-1){
+	    if((a[i]!=0)&&(a[i+1]!=0))
+	    {
+	        a[i]*=2;
+	        a[i+1]=0;
+	    }}
+	    if(a[i]!=0)
+	    {
+	        swap(a[i],a[k++]);
+	    }
+	}
+	cout<<"Array: ";
+	for(int i=0;i<n;i++)
+	{
+	    cout<<a[i]<<" ";    //output
+	}
+	cout<<endl;
+	return 0;
+}
